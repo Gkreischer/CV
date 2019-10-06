5b6a925f0bf7638299704677abdf07452209103e
Gkreischer <gustavokreischer@gmail.com>
2019-10-06 00:02:29 -0300

    Updates

---
+++ precache-manifest.3c2086527d3f81f6cd011a7435169c3f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087922c5076867315d8969a551f50523",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "bc541f1dc399a715063e",
+    "url": "/CV/static/css/2.32f3906b.chunk.css"
+  },
+  {
+    "revision": "0e9cae02013d5862c0b1",
+    "url": "/CV/static/css/main.17790c36.chunk.css"
+  },
+  {
+    "revision": "bc541f1dc399a715063e",
+    "url": "/CV/static/js/2.6eb44278.chunk.js"
+  },
+  {
+    "revision": "0e9cae02013d5862c0b1",
+    "url": "/CV/static/js/main.c4a4fbea.chunk.js"
+  },
+  {
+    "revision": "2cb4d927ace08920f0b2",
+    "url": "/CV/static/js/runtime~main.30ce5ac1.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/CV/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/CV/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/CV/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/CV/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/CV/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "7301041ceafc0b9aa8c358fcfbc512fb",
+    "url": "/CV/static/media/gustavo.7301041c.jpg"
+  }
+]);
